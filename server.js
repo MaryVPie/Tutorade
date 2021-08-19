924154a460261db77fec91cac5c6ead49120e861
MaryVPie <57842558+MaryVPie@users.noreply.github.com>
2021-08-18 20:09:59 -0700

    Revert "Updated root server.js"

--- server.js
+++ server.js
@@ -1,19 +1,16 @@
 const express = require('express');
 const session = require('express-session');
-const exphbs = require('express-handlebars');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
-
 const routes = require('./controllers');
+
 const sequelize = require('./config/connection');
-//        do we want to use helpers for use with Sequelize? 
-// const helpers = require(‘./utils/helpers');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 const sess = {
   secret: 'Super secret secret',
-  cookie: {maxAge: 60000},
+  cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
@@ -23,15 +20,11 @@ const sess = {
 
 app.use(session(sess));
 
-// const hbs = exphbs.create({ helpers });
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(routes);
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
-
