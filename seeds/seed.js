d0fd3b2df5dc672bc3919bea806d7f04944482d1
Mohamed Mesahel <82197617+MohamedMesahel@users.noreply.github.com>
2021-08-19 17:35:57 -0700

    upd

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,8 +1,8 @@
 const sequelize = require('../config/connection');
-const { User, Project } = require('../models');
+const { User, Role } = require('../models');
 
 const userData = require('./userData.json');
-const projectData = require('./projectData.json');
+const roleData = require('./roleData.json');
 
 const seedDatabase = async () => {
   await sequelize.sync({ force: true });
@@ -12,9 +12,9 @@ const seedDatabase = async () => {
     returning: true,
   });
 
-  for (const project of projectData) {
-    await Project.create({
-      ...project,
+  for (const role of roleData) {
+    await Role.create({
+      ...role,
       user_id: users[Math.floor(Math.random() * users.length)].id,
     });
   }
