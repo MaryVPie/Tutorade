db3ea8762fe6dad9e2773313f9a7d7a4c42223f4
Consolata Njeri <conso4u@gmail.com>
2021-08-21 21:05:57 -0700

    added new info

--- models/index.js
+++ models/index.js
@@ -1,10 +1,20 @@
 const User = require('./User');
 const Language = require('./Language')
-
+//const seedHelper = require('../seeds/seed')
 Language.hasMany(User, {
   onDelete: 'SET NULL'
 });
 
-User.belongsTo(Language); 
-
+User.belongsTo(Language);
+// let seedDatabase = true;
+// if (seedDatabase) {
+//   seedHelper.seedDatabase()
+//     .then(() => console.log("yey!"))
+//     .catch((err) => {
+//       console.log("sad :(");
+//       console.log("error:", err);
+//     });
+// }
 module.exports = { User, Language };
+
+
--- seeds/seed.js
+++ seeds/seed.js
@@ -1,25 +1,30 @@
 const sequelize = require('../config/connection');
-const { Language, User} = require('../models/index');
+//const { Language, User } = require('../models/index');
+
+const User = require('../models/User');
+const Language = require('../models/Language')
 
 const userData = require('./userData.json');
 const languageData = require('./languageData.json');
 
-const seedDatabase = async() => {
+const seedDatabase = async (forcequit=false) => {
     await sequelize.sync({ force: true });
 
     const language = await Language.bulkCreate(languageData, {
         individualHooks: true,
         returning: true,
     });
-console.log(language)
+    console.log(language)
 
     const users = await User.bulkCreate(userData, {
         individualHooks: true,
         returning: true,
     });
-console.log(users)
+    console.log(users)
 
-    process.exit(0);
+    if(forcequit){
+        process.exit(0);
+    }
 };
-
-seedDatabase();
+//seedDatabase();
+module.exports = { seedDatabase };
